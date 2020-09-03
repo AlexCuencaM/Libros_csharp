26f207b0610afc840f97379605e1696fa612ed97
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-09-02 23:55:02 -0500

    components

--- Libros/Form1.cs
+++ Libros/Form1.cs
@@ -16,5 +16,20 @@ namespace Libros
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
